895e151bf7f345ae1375f7bdf28eb7359e270b71
Mel Saraiva <melcr306@gmail.com>
2025-09-19 13:51:01 -0300

    feat: initialize application with CinemaService and CinemaView for user interaction

--- src/main/java/br/com/amazon/bytes/myticket/Application.java
+++ src/main/java/br/com/amazon/bytes/myticket/Application.java
@@ -1,8 +1,15 @@
 package main.java.br.com.amazon.bytes.myticket;
 
+import main.java.br.com.amazon.bytes.myticket.models.Cinema;
+import main.java.br.com.amazon.bytes.myticket.service.CinemaService;
+import main.java.br.com.amazon.bytes.myticket.view.CinemaView;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Cinema cinema = new Cinema();
+        CinemaService service = new CinemaService(cinema);
+        CinemaView view = new CinemaView(service);
+        view.start();
     }
 }
